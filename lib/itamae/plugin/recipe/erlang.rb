9639308555d253287a0cd445d5683268a7b9b467
ohr486 <ohr486@gmail.com>
2017-08-22 18:43:39 +0900

    add base file

---
+++ lib/itamae/plugin/recipe/erlang.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/erlang/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Erlang
+        # Your code goes here...
+      end
+    end
+  end
+end
