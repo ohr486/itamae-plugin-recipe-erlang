e8b7e7fa6fa4969e077f9c1176347a67ca3f38c0
ohr486 <ohr486@gmail.com>
2017-08-26 08:31:18 +0900

    fix wercker settings

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -xe
 
-wget https://releases.hashicorp.com/vagrant/1.8.4/vagrant_1.8.4_x86_64.rpm
-rpm -i vagrant_1.8.4_x86_64.rpm
+wget https://releases.hashicorp.com/vagrant/1.9.8/vagrant_1.9.8_x86_64.rpm
+rpm -i vagrant_1.9.8_x86_64.rpm
 vagrant plugin install vagrant-digitalocean
-
-# NOTE: vagrant v1.8.4 depends on bundler v1.12.5
-# https://github.com/mitchellh/vagrant/blob/v1.8.4/vagrant.gemspec#L23
-gem uninstall bundler --all --force
-gem install bundler -v 1.12.5 --no-document
 
 mkdir -m 700 -p $HOME/.ssh
 
