e9628275d6827fa91b9fddcafd676bdf7f603606
ohr486 <ohr486@gmail.com>
2017-08-26 16:15:54 +0900

    set -v option to rake

--- ci/build.sh
+++ ci/build.sh
@@ -3,5 +3,5 @@
 readonly HOST=$1
 
 vagrant up $HOST --provider=digital_ocean
-bundle exec rake itamae:$HOST
-bundle exec rake spec:$HOST
+bundle exec rake itamae:$HOST -v
+bundle exec rake spec:$HOST -v
