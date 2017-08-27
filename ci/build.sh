df05e51440cd5c9b5bc384014e2a8003c352e98c
ohr486 <ohr486@gmail.com>
2017-08-27 14:43:21 +0900

    add debug info2

--- ci/build.sh
+++ ci/build.sh
@@ -8,6 +8,8 @@ vagrant up $HOST --provider=digital_ocean
 echo `whoami`
 echo `pwd`
 echo `ls -alh ~/.ssh`
+echo `ls -alh`
+echo `vagrant ssh-config`
 
 #bundle exec rake itamae:$HOST -v
 #bundle exec rake spec:$HOST -v
