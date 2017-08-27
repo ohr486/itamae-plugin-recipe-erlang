7b891a4d1940f4fc0463829553ffa77909d3af1e
ohr486 <ohr486@gmail.com>
2017-08-27 15:32:42 +0900

    only dep install

--- ci/build.sh
+++ ci/build.sh
@@ -3,14 +3,8 @@
 readonly HOST=$1
 
 vagrant up $HOST --provider=digital_ocean
-
-# for debug
-echo `whoami`
-echo `pwd`
-echo `ls -alh ~/.ssh`
-echo `ls -alh`
-echo `vagrant ssh-config`
-echo `vagrant status`
+vagrant status
+vagrant ssh-config
 
 bundle exec rake itamae:$HOST
 bundle exec rake spec:$HOST
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,1 +1,4 @@
-include_recipe "erlang::install"
+#include_recipe "erlang::install"
+
+# for debug
+include_recipe "erlang::dependency"
